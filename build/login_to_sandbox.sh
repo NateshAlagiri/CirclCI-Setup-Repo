dd66cb4398dd3c01889f66f80932b6a59bed5d40
Natesh-Rubrik <Natesh.Alagiri@rubrik.com>
2025-03-27 20:41:11 +0530

    commit

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -1,6 +1,6 @@
 echo "Logging into Sandbox Salesforce Org"
 mkdir keys
-echo $SANDBOX_CERT_KEY -di > keys/server.key
+echo $SANDBOX_CERT_KEY | RS256 -di > keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --client-id $SANDBOX_APP_KEY --jwt-key-file keys/server.key --username $SANDBOX_USERNAME --set-default-dev-hub -a DevHub
